00ca0c1a7e78c026380008406ddd72e7f4b94f99
Sarbani Datta <sdatta@idbs.com>
2025-06-09 21:24:00 +0530

    update warehouse

--- cd_warehouse.py
+++ cd_warehouse.py
@@ -4,4 +4,4 @@ class CDWarehouse:
 
     def add_cd_in_stock(self, title, quantity):
         if quantity:
-            self.inventory[title] = self.inventory.get{title, 0} + quantity
+            self.inventory[title] = self.inventory.get(title, 0) + quantity
